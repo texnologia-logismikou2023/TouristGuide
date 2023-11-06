bcc5ba03802936c16f40d9afc7d8afc8a0c99f19
Alexandros Kalesis <148329298+alexandroskalesis@users.noreply.github.com>
2023-11-07 00:32:56 +0200

    Add files via upload

---
+++ TouristGuide/app/src/main/java/com/example/touristguide/MapActivity.java
@@ -0,0 +1,29 @@
+package com.example.touristguide;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class {
+
+        }
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // ορισμός τοποθεσίας και λοιπές ρυθμίσεις χάρτη
+    }
+}
